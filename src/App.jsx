187048f6107e3ec9a0e5a65a9e03ffbf96d5f1af
georgeding <dingchangzhao@hotmail.com>
2023-06-09 00:03:46 +0800

    router installation; initial draft of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import GameTitles from "./GameTitles";
+import SteamRatings from "./SteamRatings";
+import SalePrices from "./SalePrices";
 
 function App() {
-  const [games, setGames] = useState()
+  const [games, setGames] = useState();
   useEffect(() => {
     async function getGames() {
-      const response = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`);
+      const response = await fetch(
+        `https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`
+      );
       const jsonData = await response.json();
       setGames(jsonData);
     }
@@ -13,10 +19,15 @@ function App() {
   }, []);
   return (
     <>
-      Test
-      <p>{JSON.stringify(games)}</p>
+      <h1>Welcome To PlayHQ</h1>
+      <Routes>
+        <Route path="/games" element={<GameTitles />} />
+        <Route path="/ratings" element={<SteamRatings />} />
+        <Route path="/saleprices" element={<SalePrices />} />
+      </Routes>
+      {/* <p>{JSON.stringify(games)}</p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/GameTitles.jsx
@@ -0,0 +1,7 @@
+export default function GameTitles() {
+  return (
+    <>
+      <h2>Game Titles</h2>
+    </>
+  );
+}

a2afb0e30eecb7240926e7e98a807aee14f93600
georgeding <dingchangzhao@hotmail.com>
2023-06-15 16:39:41 +0800

    comment out airtable section

--- src/Components/Routes/Homepage.jsx
+++ src/Components/Routes/Homepage.jsx
@@ -11,35 +11,35 @@ export default function Homepage({
   gamesByReleaseDate,
   unixTimeStamp,
 }) {
-  const [testAir, setTestAir] = useState([]);
+  // const [testAir, setTestAir] = useState([]);
 
-  const handleTestAir = () => {
-    async function getAirTable() {
-      const response = await fetch(
-        `https://api.airtable.com/v0/applqK2pr4BxQlJ07/PlayHQ`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer key1Adz0lNfrpm3Y2`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const jsonData = await response.json();
-      setTestAir(jsonData.records);
-      console.log(jsonData.records);
-    }
-    getAirTable();
-  };
+  // const handleTestAir = () => {
+  //   async function getAirTable() {
+  //     const response = await fetch(
+  //       `https://api.airtable.com/v0/applqK2pr4BxQlJ07/PlayHQ`,
+  //       {
+  //         method: "GET",
+  //         headers: {
+  //           Authorization: `Bearer key1Adz0lNfrpm3Y2`,
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     const jsonData = await response.json();
+  //     setTestAir(jsonData.records);
+  //     console.log(jsonData.records);
+  //   }
+  //   getAirTable();
+  // };
 
   return (
     <>
-      <>
+      {/* <>
         <button onClick={handleTestAir}>Test</button>
         {testAir.map((item, index) => (
           <div key={index}>{JSON.stringify(item)}</div>
         ))}
-      </>
+      </> */}
 
       <div className="homepage">
         <CheapGamesGroup gamesByPrice={gamesByPrice} />
